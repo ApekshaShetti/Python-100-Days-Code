d21df7d0baf51e8ca8c99b7c17ad33474f4cea32
Apeksha <apekshashetti1012@gmail.com>
2022-01-15 13:24:57 +0530

    Still Adding

---
+++ .py
@@ -0,0 +1,31 @@
+# class MenuItem(name,cost,ingredients):
+
+
+# class Menu():
+#     get_items():
+
+
+#     find_drink(order_name):
+
+
+# class CoffeeMaker():
+#     report():
+#     print("Water: 300ml,
+#         Milk: 200ml,
+#         Coffee: 100g")
+
+#     is_resource_sufficeint(drink):
+
+
+#     make_coffee(order):
+
+    
+class MoneyMachine():
+    report()
+    print("Money: $0")
+
+    make_payment()
+
+my_money_machine=MoneyMachine()
+
+my_money_machine.report()
