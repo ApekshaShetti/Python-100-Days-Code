3de478d6a9b7951d0086f9f6edd6260e8eec51da
Apeksha <apekshashetti1012@gmail.com>
2022-05-12 20:09:48 +0530

    Day_35

--- Day_35/rain_alert/main.py
+++ Day_35/rain_alert/main.py
@@ -1,13 +1,16 @@
 import requests
 
-OWN_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "a1acc648389bdea42b4a20f0e1376235"
 
 weather_params ={
     "lat": 18.945128,
     "long": 18.945128,
-    "appid": api_key
+    "appid": api_key,
+    "exclude":"current,minutely,daily"
 }
 
-response = requests.get(OWN_Endpoint,params=weather_params)
-print(response.json())
+response = requests.get(OWM_Endpoint,params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+print(weather_data)
