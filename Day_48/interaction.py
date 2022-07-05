a5a36340302099dd412c3336461ec62cabdaa299
Apeksha <apekshashetti1012@gmail.com>
2022-07-05 16:09:15 +0530

    Day 48

--- Day_48/interaction.py
+++ Day_48/interaction.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 chrome_driver_path = "D:\Python-100-Days-Code\chromedriver_win32\chromedriver.exe"
-driver = webdriver.Chrome(chrome_driver_path)
+s = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=s)
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-article_count = driver.find_element_by_css_selector("#articlecount a")
+article_count = driver.find_elements(By.CSS_SELECTOR,"#articlecount a")
 article_count.click()
 
-all_portals = driver.find_element_by_link_text("All portals")
+all_portals = driver.find_elements(By.LINK_TEXT, "All portals")
 all_portals.click()
 
-search = driver.find_element_by_name("search")
+search = driver.find_elements(By.CLASS_NAME,"search")
 search.send_keys("Python")
 search.send_keys(Keys.ENTER)
--- Day_48/main.py
+++ Day_48/main.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 chrome_driver_path = "D:\Python-100-Days-Code\chromedriver_win32\chromedriver.exe"
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+s = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=s)
 
 driver.get("https://www.python.org/")
 
-event_times = driver.find_elements_by_css_selector(".event-widget time")
-event_names = driver.find_elements_by_css_selector(".event-widget li a")
+# event_times = driver.find_elements_by_css_selector(".event-widget time")
+event_times = driver.find_elements(By.CSS_SELECTOR,".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR,".event-widget li a")
+# event_names = driver.find_elements_by_css_selector(".event-widget li a")
 events = {}
+
 
 for n in range(len(event_times)):
     events[n] ={
