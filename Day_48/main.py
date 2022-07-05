e62e001d09974fbf4c1d2a4ee68965d23b9107f8
Apeksha <apekshashetti1012@gmail.com>
2022-07-05 12:36:43 +0530

    Day 48

--- Day_48/main.py
+++ Day_48/main.py
@@ -3,9 +3,21 @@ from selenium import webdriver
 chrome_driver_path = "D:\Python-100-Days-Code\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.amazon.com")
+driver.get("https://www.python.org/")
 
-driver.close()
+event_times = driver.find_elements_by_css_selector(".event-widget time")
+event_names = driver.find_elements_by_css_selector(".event-widget li a")
+events = {}
+
+for n in range(len(event_times)):
+    events[n] ={
+        "time": event_times[n].text,
+        "name": event_names[n].text
+    }
+print(events)
+
+
+# driver.close()
 # closes only the active tab or the current tab of chrome
 driver.quit()
 # closes the browser which is chrome
