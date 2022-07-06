106132c3d4698b4284f8adf82661686b6595e0dc
Apeksha <apekshashetti1012@gmail.com>
2022-07-06 18:47:09 +0530

    Day 48

--- Day_48/interaction.py
+++ Day_48/interaction.py
@@ -7,14 +7,27 @@ chrome_driver_path = "D:\Python-100-Days-Code\chromedriver_win32\chromedriver.ex
 s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
 
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.get("http://secure-retreat-92358.herokuapp.com/")
 
-article_count = driver.find_elements(By.CSS_SELECTOR,"#articlecount a")
-article_count.click()
+first_name = driver.find_elements(By.CLASS_NAME,"fName")
+first_name.send_keys("Apeksha")
+last_name = driver.find_elements(By.CLASS_NAME,"lName")
+last_name.send_keys("Shetti")
+email = driver.find_elements(By.CLASS_NAME,"email")
+email.send_keys("shetti1012@gmail.com")
 
-all_portals = driver.find_elements(By.LINK_TEXT, "All portals")
-all_portals.click()
+submit = driver.find_elements(By.CSS_SELECTOR,"form button")
+submit.click()
 
-search = driver.find_elements(By.CLASS_NAME,"search")
-search.send_keys("Python")
-search.send_keys(Keys.ENTER)
+
+# article_count = driver.find_elements(By.CSS_SELECTOR,"#articlecount a")
+# article_count.click()
+
+# all_portals = driver.find_elements(By.LINK_TEXT, "All portals")
+# all_portals.click()
+
+# search = driver.find_elements(By.CLASS_NAME,"search")
+# search.send_keys("Python")
+# search.send_keys(Keys.ENTER)
+
+
