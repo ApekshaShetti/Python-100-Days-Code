fb8d7d724d1e61b09f46648aa1351f09313ec4c1
Apeksha <apekshashetti1012@gmail.com>
2022-07-06 18:14:59 +0530

    Day 46

--- Day_46 Spotify Playlist/main.py
+++ Day_46 Spotify Playlist/main.py
@@ -6,13 +6,14 @@ from spotipy.oauth2 import SpotifyOAuth
 
 SPOTIPY_CLIENT_ID='99ed42577881444cb358e99c74602896'
 SPOTIPY_CLIENT_SECRET='ae9c29c45f5845d1bc6d0725fcbf6c82'
-URL = "https://www.billboard.com/charts/billboard-global-200/"
+URL = "https://www.billboard.com/charts/hot-100/"
 user = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
-        redirect_uri="http://example.com",
+        # redirect_uri="http://example.com",
+        redirect_uri="https://example.com/callback",
         client_id=SPOTIPY_CLIENT_ID,
         client_secret=SPOTIPY_CLIENT_SECRET,
         show_dialog=True,
@@ -24,14 +25,16 @@ response = requests.get(URL+user)
 data = response.text
 
 soup = BeautifulSoup(data,'html.parser')
-songs = soup.find_all("span", class_="chart-element__information__song")
+# songs = soup.find_all("span", class_="chart-element__information__song")
+songs = soup.find_all(name="h3", class_="a-no-trucate")
+print(songs)
 
 user_id = sp.current_user()["id"]
-song_names = song_names = [song.getText() for song in song_names_spans]
+song_names = [song.getText() for song in songs]
 
 
 song_uris = []
-year = date.split("-")[0]
+year = user.split("-")[0]
 for song in song_names:
     result = sp.search(q=f"track:{song} year:{year}", type="track")
     print(result)
@@ -43,14 +46,6 @@ for song in song_names:
 
 
 
-playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+playlist = sp.user_playlist_create(user=user_id, name=f"{user} Billboard 100", public=False)
 
 sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
-
-
-
-
-
-
-
-
